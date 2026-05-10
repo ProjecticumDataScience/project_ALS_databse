a6a76eaada3977a26c2ac76ac1406e3c4679df99
LTNengels <1854660@hu-p-dataanalysis01.sxa4wv524ayerorjvggj0oli3c.ax.internal.cloudapp.net>
2026-05-10 13:45:24 +0200

    v1

--- scripts/querychat_pipeline/setup.R
+++ scripts/querychat_pipeline/setup.R
@@ -1,7 +1,7 @@
 library(rollama)
 
 # This checks that Ollama is running and the model is available
-pull_model("llama3")       # downloads the model if not already present
+
 
 # A simple test to verify it works
 query("What is ALS?", model = "llama3")
